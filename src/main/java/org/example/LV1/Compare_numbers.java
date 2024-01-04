d33cdf1d8c4060e24560c85e08f5f3bfabaa4053
SerenityZenDev <tjrqo2000@gmail.com>
2024-01-04 09:54:42 +0900

    숫자 비교하기

---
+++ src/main/java/org/example/LV1/Compare_numbers.java
@@ -0,0 +1,17 @@
+package org.example.LV1;
+
+public class Compare_numbers {
+    public static int solution(int num1, int num2) {
+
+        if(num1 == num2){
+            return 1;
+        }else{
+            return -1;
+        }
+
+    }
+    public static void main(String[] args) {
+        System.out.println(solution(10,10));
+        System.out.println(solution(10,20));
+    }
+}
--- src/main/java/org/example/LV1/age_output.java
+++ src/main/java/org/example/LV1/age_output.java
@@ -2,6 +2,7 @@ package org.example.LV1;
 
 public class age_output {
     public static int solution(int age) {
+        // 2022년을 기준으로 함
         int answer = 2022 - age + 1;
         return answer;
     }
