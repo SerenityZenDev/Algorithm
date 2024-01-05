6a78c77a1a49660cfbfaaef52cdbeb06808363c8
SerenityZenDev <tjrqo2000@gmail.com>
2024-01-05 09:39:58 +0900

    평균구하기

---
+++ src/main/java/org/example/LV2/Find_the_average.java
@@ -0,0 +1,19 @@
+package org.example.LV2;
+
+public class Find_the_average {
+    public double solution(int[] arr) {
+        double answer = 0;
+        int length = arr.length;
+        for(int i=0; i<length; i++){
+            answer += arr[i];
+        }
+        answer /= length;
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Find_the_average solution = new Find_the_average();
+        int arr[] = {1,2,3,4,5,6};
+        System.out.println(solution.solution(arr));
+    }
+}
