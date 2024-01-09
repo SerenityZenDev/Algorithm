5f9bfe41486e73af36d492bd399cafdc474f0d84
SerenityZenDev <tjrqo2000@gmail.com>
2024-01-09 09:07:41 +0900

    문자열을 정수로 바꾸기

---
+++ src/main/java/org/example/LV2/string_to_integer.java
@@ -0,0 +1,15 @@
+package org.example.LV2;
+
+public class string_to_integer {
+    public int solution(String s) {
+        int answer = 0;
+        answer = Integer.parseInt(s);
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        string_to_integer solution = new string_to_integer();
+        System.out.println(solution.solution("1234"));
+        System.out.println(solution.solution("-1234"));
+    }
+}
