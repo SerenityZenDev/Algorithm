51dbb9e1855af9db688cacd4065e90179b1c0256
SerenityZenDev <tjrqo2000@gmail.com>
2024-01-09 09:14:04 +0900

    정수 제곱근 판별

---
+++ src/main/java/org/example/LV2/Determine_integer_square_root.java
@@ -0,0 +1,20 @@
+package org.example.LV2;
+
+public class Determine_integer_square_root {
+    public long solution(long n) {
+        long answer = 0;
+        long sqrt = (long)Math.sqrt(n);
+        if(sqrt * sqrt == n){
+            answer = (sqrt+1)*(sqrt+1);
+        }else{
+            answer = -1;
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Determine_integer_square_root solution = new Determine_integer_square_root();
+        System.out.println(solution.solution(121));
+        System.out.println(solution.solution(3));
+    }
+}
