567d9c9e75dcc46e47f15018f923206455496d2f
SerenityZenDev <tjrqo2000@gmail.com>
2024-01-11 09:29:41 +0900

    없는 숫자 더하기

---
+++ src/main/java/org/example/LV2/Add_missing_numbers.java
@@ -0,0 +1,20 @@
+package org.example.LV2;
+
+public class Add_missing_numbers {
+    public static int solution(int[] numbers) {
+        int answer = 0;
+        for(int i=0; i<10; i++){
+            answer += i;
+        }
+
+        for(int i=0; i<numbers.length; i++){
+            answer -= numbers[i];
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int[] numbers = {1,2,3,4,6,7,8,0};
+        System.out.println(solution(numbers));
+    }
+}
