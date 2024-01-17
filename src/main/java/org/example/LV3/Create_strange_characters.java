7f55b60acaea0d83619dd5051ef6de177e54c67a
SerenityZenDev <tjrqo2000@gmail.com>
2024-01-17 09:27:51 +0900

    삼총사

--- src/main/java/org/example/LV3/Create_strange_characters.java
+++ src/main/java/org/example/LV3/Create_strange_characters.java
@@ -1,6 +1,22 @@
 package org.example.LV3;
 
 public class Create_strange_characters {
+    /*
+    public String solution(String s) {
+        String answer = "";
+        char[] string = s.trim().toCharArray();
+        for (int i=0; i<string.length; i++){
+            if(i%2 == 0 && string[i] > 96 && string[i] < 123){
+                answer += (char)(string[i] - 32);
+            }else if(i%2 == 1 && string[i] < 96 && string[i] > 64){
+                answer += (char)(string[i] + 32);
+            }else{
+                answer += string[i];
+            }
+        }
+        return answer;
+    }
+     */
     public static String solution(String s) {
         String answer = "";
         int cnt = 0;
