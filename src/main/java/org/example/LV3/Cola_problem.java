cceb30f738603ea53e50fa2aa1b767776571325d
SerenityZenDev <tjrqo2000@gmail.com>
2024-01-24 09:29:44 +0900

    콜라 문제

---
+++ src/main/java/org/example/LV3/Cola_problem.java
@@ -0,0 +1,21 @@
+package org.example.LV3;
+
+public class Cola_problem {
+    public static int solution(int a, int b, int n) {
+        int answer = getCola(a,b,n);
+        return answer;
+    }
+
+    public static int getCola(int a, int b, int n){
+        int cola = 0;
+        while(true){
+            if (n < a) break;
+            else {
+                int exchangedCola = n / a * b;
+                cola += exchangedCola;
+                n = exchangedCola + n % a;
+            }
+        }
+        return cola;
+    }
+}
