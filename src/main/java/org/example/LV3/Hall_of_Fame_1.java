ddcd262b7dfcf71823d9fcc6ad31439a8c8fa25b
SerenityZenDev <tjrqo2000@gmail.com>
2024-01-25 09:22:54 +0900

    명예의 전당(1)

---
+++ src/main/java/org/example/LV3/Hall_of_Fame_1.java
@@ -0,0 +1,30 @@
+package org.example.LV3;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Hall_of_Fame_1 {
+    public static int[] solution(int k, int[] score) {
+        int [] answer = new int[score.length];
+
+        List<Integer> rank = new ArrayList<>();
+
+        for (int i=0; i< score.length; i++){
+            rank.add(score[i]);
+
+            if (rank.size() > k){
+                rank.remove(Collections.min(rank));
+            }
+
+            answer[i] = Collections.min(rank);
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int [] a = {10, 100, 20, 150, 1, 100, 200};
+        solution(3,a);
+    }
+}
