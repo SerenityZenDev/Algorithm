84f20af8b36d592e41a2ab29e473e562367d5b00
SerenityZenDev <tjrqo2000@gmail.com>
2024-01-30 09:44:25 +0900

    기사단원의 무기(/)

---
+++ src/main/java/org/example/LV3/Templars_Weapon.java
@@ -0,0 +1,24 @@
+package org.example.LV3;
+
+public class Templars_Weapon {
+    public static int solution(int number, int limit, int power) {
+        int answer = 0;
+
+        for(int i=1;i<=number;i++){
+            int cnt = 0;
+            for(int j=1;j*j<=i;j++){
+                if(j*j==i) cnt++;
+                else if(i%j==0) cnt+=2;
+            }
+
+            if(cnt>limit) cnt = power;
+            answer += cnt;
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution(5,3,2));
+    }
+}
