822056bd1ccca004880585c5437a1c6dde3d18d1
SerenityZenDev <tjrqo2000@gmail.com>
2024-01-31 09:49:31 +0900

    옹알이2(/)

---
+++ src/main/java/org/example/LV3/Babbling.java
@@ -0,0 +1,24 @@
+package org.example.LV3;
+
+public class Babbling {
+    public static int solution(String[] babbling) {
+        int answer = 0;
+
+        String[] can = {"aya", "ye", "woo", "ma"};
+
+        for (int i=0; i< babbling.length; i++){
+            if (babbling[i].contains("ayaaya") || babbling[i].contains("yeye") || babbling[i].contains("woowoo") || babbling[i].contains("mama")) continue;
+            for (int j=0; j< can.length;j++){
+                babbling[i] = babbling[i].replace(can[j], " ");
+            }
+            babbling[i] = babbling[i].replace(" ", "");
+            if (babbling[i].length() == 0) answer++;
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        String[] input = {"aya", "yee", "u"};
+        System.out.println(solution(input));
+    }
+}
