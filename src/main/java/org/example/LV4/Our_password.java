84ce5ab8eb891bfca2a36b67e447f4f55f99dee1
SerenityZenDev <tjrqo2000@gmail.com>
2024-02-06 09:25:57 +0900

    둘만의 암호

---
+++ src/main/java/org/example/LV4/Our_password.java
@@ -0,0 +1,19 @@
+package org.example.LV4;
+
+public class Our_password {
+    public String solution(String s, String skip, int index) {
+        String answer = "";
+
+        for (int i=0; i<s.length(); i++){
+            char c = s.charAt(i);
+            for (int j=0; j<index; j++){
+                c+=1;
+                if (c > 'z') c-= 26;
+                if (skip.contains(String.valueOf(c))) j--;
+            }
+            answer += c;
+        }
+
+        return answer;
+    }
+}
