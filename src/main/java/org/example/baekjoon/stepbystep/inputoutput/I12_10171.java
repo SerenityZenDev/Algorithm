ed5af6d1c2e9005d21cfffd787da2585bf04eb31
SerenityZenDev <tjrqo2000@gmail.com>
2024-03-03 11:24:26 +0900

    :sparkles: baekjoon - 고양이

---
+++ src/main/java/org/example/baekjoon/stepbystep/inputoutput/I12_10171.java
@@ -0,0 +1,12 @@
+package org.example.baekjoon.stepbystep.inputoutput;
+
+public class I12_10171 {
+    public static void main(String[] args) {
+        System.out.print(
+            "\\    /\\\n"
+                + " )  ( ')\n"
+                + "(  /  )\n"
+                + " \\(__)|"
+        );
+    }
+}
