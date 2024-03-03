88ae1daaaf72a6d13a906028cf7acb4003c4ef25
SerenityZenDev <tjrqo2000@gmail.com>
2024-03-03 11:40:30 +0900

    :sparkles: baekjoon - 윤년

---
+++ src/main/java/org/example/baekjoon/stepbystep/conditional/C3_2753.java
@@ -0,0 +1,24 @@
+package org.example.baekjoon.stepbystep.conditional;
+
+import java.util.Scanner;
+
+public class C3_2753 {
+    /*
+    윤년의 조건
+    1. 연도가 4의 배수 (무조건)
+    2. 100의 배수가 아닐 때 OR 400의 배수일 때
+     */
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int year = in.nextInt();
+
+        if (year % 4 == 0) {
+            if (year % 100 != 0 || year % 400 == 0) {
+                System.out.println(1);
+                System.exit(0);
+            }
+        }
+        System.out.println(0);
+
+    }
+}
