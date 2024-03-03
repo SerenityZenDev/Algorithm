65e67d55fcb4dbd448e6d513df7e5bd17ac3a755
SerenityZenDev <tjrqo2000@gmail.com>
2024-03-03 11:05:28 +0900

    :sparkles: baekjoon - 나머지

---
+++ src/main/java/org/example/baekjoon/stepbystep/inputoutput/I9_10430.java
@@ -0,0 +1,19 @@
+package org.example.baekjoon.stepbystep.inputoutput;
+
+import java.util.Scanner;
+
+public class I9_10430 {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int A = input.nextInt();
+        int B = input.nextInt();
+        int C = input.nextInt();
+
+        System.out.println((A + B) % C);
+        System.out.println(((A % C) + (B % C)) % C);
+        System.out.println((A * B) % C);
+        System.out.println(((A % C) * (B % C)) % C);
+    }
+
+}
