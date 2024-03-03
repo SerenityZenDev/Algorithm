a5b3f84599c1c21fe56a8d900e076aa9e94e963a
SerenityZenDev <tjrqo2000@gmail.com>
2024-03-03 10:47:54 +0900

    :sparkles: baekjoon-A*B

---
+++ src/main/java/org/example/baekjoon/stepbystep/inputoutput/I4_10998.java
@@ -0,0 +1,14 @@
+package org.example.baekjoon.stepbystep.inputoutput;
+
+import java.util.Scanner;
+
+public class I4_10998 {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        int m = input.nextInt();
+        System.out.println(n*m);
+    }
+
+}
