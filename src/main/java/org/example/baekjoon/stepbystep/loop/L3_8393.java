259feb2ae1aebbb1204fe1b8388dc7c9112c61ee
SerenityZenDev <tjrqo2000@gmail.com>
2024-03-09 19:23:16 +0900

    :sparkles: baekjoon - 합

---
+++ src/main/java/org/example/baekjoon/stepbystep/loop/L3_8393.java
@@ -0,0 +1,16 @@
+package org.example.baekjoon.stepbystep.loop;
+
+import java.util.Scanner;
+
+public class L3_8393 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            sum += i;
+        }
+        System.out.println(sum);
+    }
+}
