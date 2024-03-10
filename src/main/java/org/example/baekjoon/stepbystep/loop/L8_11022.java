141201af074986a3700b3ad66c772466593ee40d
SerenityZenDev <tjrqo2000@gmail.com>
2024-03-10 10:38:36 +0900

    :sparkles: baekjoon - A+B - 8

---
+++ src/main/java/org/example/baekjoon/stepbystep/loop/L8_11022.java
@@ -0,0 +1,31 @@
+package org.example.baekjoon.stepbystep.loop;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class L8_11022 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int count = Integer.parseInt(br.readLine());
+
+        StringTokenizer st;
+
+        for (int i = 1; i <= count; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            bw.write( "Case #" + i + ": " + + a + " + " + b + " = " + (a+b) + "\n");
+        }
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+}
