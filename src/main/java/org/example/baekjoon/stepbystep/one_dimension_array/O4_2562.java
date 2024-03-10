a02e78569dd2c7a75411bec59835a8147a48e834
SerenityZenDev <tjrqo2000@gmail.com>
2024-03-10 12:26:58 +0900

    :sparkles: baekjoon - 최댓값

---
+++ src/main/java/org/example/baekjoon/stepbystep/one_dimension_array/O4_2562.java
@@ -0,0 +1,26 @@
+package org.example.baekjoon.stepbystep.one_dimension_array;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class O4_2562 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int max = 0;
+        int index = 0;
+
+        for (int i=0; i<9; i++) {
+            int value = Integer.parseInt(br.readLine());
+            if (value > max) {
+                max = value;
+                index = i;
+            }
+
+        }
+        System.out.println(max);
+        System.out.println(index + 1);
+    }
+
+}
