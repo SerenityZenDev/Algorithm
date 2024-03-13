35cf254b83e9e1c65460442a5e5059614fb6c2fb
SerenityZenDev <tjrqo2000@gmail.com>
2024-03-13 09:33:53 +0900

    :sparkles: sparta - 크레인 인형뽑기

--- src/main/java/org/example/sparta/daily/D7_Drap_doll.java
+++ src/main/java/org/example/sparta/daily/D7_Drap_doll.java
@@ -3,7 +3,7 @@ package org.example.sparta.daily;
 import java.util.Stack;
 
 public class D7_Drap_doll {
-    public int solution(int[][] board, int[] moves) {
+    public static int solution(int[][] board, int[] moves) {
         int result = 0;
 
         Stack<Integer> stack = new Stack<>();
@@ -24,6 +24,19 @@ public class D7_Drap_doll {
             }
         }
         return result;
+    }
+
+    public static void main(String[] args) {
+        int[][] board = {
+            {0,0,0,0,0},
+            {0,0,1,0,3},
+            {0,2,5,0,1},
+            {4,2,4,4,2},
+            {3,5,1,3,1}
+        };
+        int[] moves = {1,5,3,5,1,2,1,4};
+
+        System.out.println(solution(board, moves));
     }
 
 
