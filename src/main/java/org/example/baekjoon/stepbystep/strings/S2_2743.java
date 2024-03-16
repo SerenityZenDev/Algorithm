063e0204c8d8e2e949e947585c3e15fdba2f42a3
SerenityZenDev <tjrqo2000@gmail.com>
2024-03-16 11:12:38 +0900

    :sparkles: baekjoon - 단어 길이 재기

---
+++ src/main/java/org/example/baekjoon/stepbystep/strings/S2_2743.java
@@ -0,0 +1,16 @@
+package org.example.baekjoon.stepbystep.strings;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class S2_2743 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String word = br.readLine();
+
+        System.out.println(word.length());
+    }
+}
