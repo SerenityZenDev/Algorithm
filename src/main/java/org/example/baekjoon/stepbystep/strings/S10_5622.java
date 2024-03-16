9ca37abc2f928e6ca8f16a043d6c03b3c737caeb
SerenityZenDev <tjrqo2000@gmail.com>
2024-03-16 19:06:04 +0900

    :sparkles: baekjoon - 다이얼

---
+++ src/main/java/org/example/baekjoon/stepbystep/strings/S10_5622.java
@@ -0,0 +1,32 @@
+package org.example.baekjoon.stepbystep.strings;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class S10_5622 {
+
+    public static void main(String[] args) {
+       int[] alphabet = {
+           2,2,2,
+           3,3,3,
+           4,4,4,
+           5,5,5,
+           6,6,6,
+           7,7,7,7,
+           8,8,8,
+           9,9,9,9
+       };
+
+       Scanner sc = new Scanner(System.in);
+       String word = sc.nextLine();
+       int sum = 0;
+
+       for (int i = 0; i < word.length(); i++) {
+           sum += alphabet[word.charAt(i) - 'A'];
+       }
+
+        System.out.println(sum + word.length());
+
+    }
+}
