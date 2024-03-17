15565dc80cff3797c6b323c8c89411f29d0cee07
SerenityZenDev <tjrqo2000@gmail.com>
2024-03-17 09:56:17 +0900

    :sparkles: baekjoon - 새싹

---
+++ src/main/java/org/example/baekjoon/stepbystep/LV6_심화_1/H1_25083.java
@@ -0,0 +1,13 @@
+package org.example.baekjoon.stepbystep.LV6_심화_1;
+
+public class H1_25083 {
+
+    public static void main(String[] args) {
+        System.out.println("         ,r'\"7\n"
+            + "r`-_   ,'  ,/\n"
+            + " \\. \". L_r'\n"
+            + "   `~\\/\n"
+            + "      |\n"
+            + "      |");
+    }
+}
