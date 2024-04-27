a216057c2cb0c4793fe8c252109ae987010844b5
SerenityZenDev <tjrqo2000@gmail.com>
2024-04-27 16:52:01 +0900

    :sparkles: baekjoon(2501) - 약수 구하기

--- src/main/java/org/example/baekjoon/stepbystep/LV9_약수_배수와_소수/Q2_2501.java
+++ src/main/java/org/example/baekjoon/stepbystep/LV9_약수_배수와_소수/Q2_2501.java
@@ -1,2 +1,30 @@
-package org.example.baekjoon.stepbystep.LV9_약수_배수와_소수;public class Q2_2501 {
+package org.example.baekjoon.stepbystep.LV9_약수_배수와_소수;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Q2_2501 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String input = br.readLine();
+
+        int n = Integer.parseInt(input.split(" ")[0]);
+        int m = Integer.parseInt(input.split(" ")[1]);
+
+        for (int i = 1; i <= n; i++) {
+            if(n % i == 0) m--;
+            if (m == 0) {
+                System.out.println(i);
+                break;
+            }
+        }
+        if (m != 0) {
+            System.out.println(0);
+        }
+
+
+    }
 }
