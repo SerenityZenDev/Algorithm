cbdd0fb2f0351d196160632fcb086046638af5cf
SerenityZenDev <tjrqo2000@gmail.com>
2024-04-28 10:59:46 +0900

    :sparkles: baekjoon(11653) - 소인수분해

---
+++ src/main/java/org/example/baekjoon/stepbystep/LV9_약수_배수와_소수/Q6_11653.java
@@ -0,0 +1,26 @@
+package org.example.baekjoon.stepbystep.LV9_약수_배수와_소수;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+public class Q6_11653 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        for (int i = 2; i < n; i++) {
+            while(n % i == 0) {
+                System.out.println(i);
+                n /= i;
+            }
+        }
+        if (n != 1){
+            System.out.println(n);
+        }
+    }
+
+}
