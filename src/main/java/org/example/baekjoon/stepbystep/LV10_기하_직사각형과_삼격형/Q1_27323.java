65c3774d696c5433330209edcc5f64d9f06b99f9
SerenityZenDev <tjrqo2000@gmail.com>
2024-05-02 20:46:18 +0900

    :sparkles: baekjoon(27323) - 직사각형

---
+++ src/main/java/org/example/baekjoon/stepbystep/LV10_기하_직사각형과_삼격형/Q1_27323.java
@@ -0,0 +1,15 @@
+package org.example.baekjoon.stepbystep.LV10_기하_직사각형과_삼격형;
+
+import java.util.Scanner;
+
+public class Q1_27323 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int row = scanner.nextInt();
+        int col = scanner.nextInt();
+
+        System.out.println(row * col);
+    }
+}
