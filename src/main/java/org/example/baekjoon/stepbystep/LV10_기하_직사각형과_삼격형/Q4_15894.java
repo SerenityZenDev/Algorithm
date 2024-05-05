0b54acc781f0b2c0a20f1fbc3503771e3cb3877a
SerenityZenDev <tjrqo2000@gmail.com>
2024-05-05 11:19:55 +0900

    :sparkles: baekjoon(15894) - 수학은 체육과목 입니다

--- src/main/java/org/example/baekjoon/stepbystep/LV10_기하_직사각형과_삼격형/Q4_15894.java
+++ src/main/java/org/example/baekjoon/stepbystep/LV10_기하_직사각형과_삼격형/Q4_15894.java
@@ -1,2 +1,15 @@
-package org.example.baekjoon.stepbystep.LV10_기하_직사각형과_삼격형;public class Q4_15894 {
+package org.example.baekjoon.stepbystep.LV10_기하_직사각형과_삼격형;
+
+import java.util.Scanner;
+
+public class Q4_15894 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+        Long result = 3L * n + n;
+
+        System.out.println(result);
+    }
 }
