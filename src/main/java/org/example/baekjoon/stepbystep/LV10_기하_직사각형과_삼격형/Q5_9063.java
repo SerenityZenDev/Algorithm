79d720fd8c05d30ad8b2eca2dac7333dec871dba
SerenityZenDev <tjrqo2000@gmail.com>
2024-05-07 09:48:23 +0900

    :sparkles: baekjoon(9063) - 대지

--- src/main/java/org/example/baekjoon/stepbystep/LV10_기하_직사각형과_삼격형/Q5_9063.java
+++ src/main/java/org/example/baekjoon/stepbystep/LV10_기하_직사각형과_삼격형/Q5_9063.java
@@ -1,2 +1,34 @@
-package org.example.baekjoon.stepbystep.LV10_기하_직사각형과_삼격형;public class Q5_9063 {
+package org.example.baekjoon.stepbystep.LV10_기하_직사각형과_삼격형;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Q5_9063 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        if (n < 2){
+            System.out.println(0);
+            System.exit(0);
+        }
+
+        int minX = Integer.MAX_VALUE, minY = Integer.MAX_VALUE;
+        int maxX = -10000, maxY = -10000;
+
+        for (int i = 0; i < n; i++) {
+            String line = br.readLine();
+            int x = Integer.parseInt(line.split(" ")[0]);
+            int y = Integer.parseInt(line.split(" ")[1]);
+
+            minX = Math.min(minX, x);
+            minY = Math.min(minY, y);
+            maxX = Math.max(maxX, x);
+            maxY = Math.max(maxY, y);
+        }
+        System.out.println((maxX - minX) * (maxY - minY));
+    }
 }
