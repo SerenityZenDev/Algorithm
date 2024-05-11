2aad4363972099fd87629f591a03636290e50f8f
SerenityZenDev <tjrqo2000@gmail.com>
2024-05-11 10:51:35 +0900

    :sparkles: baekjoon(24264) - 알고리즘 수업 - 알고리즘의 수행 시간 3

---
+++ src/main/java/org/example/baekjoon/stepbystep/LV11_시간_복잡도/Q3_24264.java
@@ -0,0 +1,16 @@
+package org.example.baekjoon.stepbystep.LV11_시간_복잡도;
+
+import java.util.Scanner;
+
+public class Q3_24264 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        Long a = sc.nextLong();
+
+        System.out.println(a*a);
+        System.out.println(2);
+
+    }
+}
