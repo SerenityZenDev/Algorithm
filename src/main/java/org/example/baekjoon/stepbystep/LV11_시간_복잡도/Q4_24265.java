ac286d86f12ba6f469792f7b1822f9f3e96dd9cd
SerenityZenDev <tjrqo2000@gmail.com>
2024-05-14 09:26:19 +0900

    :sparkles: baekjoon(24265) - 알고리즘 수업 - 알고리즘의 수행 시간 4

--- src/main/java/org/example/baekjoon/stepbystep/LV11_시간_복잡도/Q4_24265.java
+++ src/main/java/org/example/baekjoon/stepbystep/LV11_시간_복잡도/Q4_24265.java
@@ -1,2 +1,17 @@
-package org.example.baekjoon.stepbystep.LV11_시간_복잡도;public class Q4_24265 {
+package org.example.baekjoon.stepbystep.LV11_시간_복잡도;
+
+import java.util.Scanner;
+
+public class Q4_24265 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        long a = sc.nextLong();
+        long result = (a * (a-1))/2;
+
+        System.out.println(result);
+        System.out.println(2);
+    }
+
 }
