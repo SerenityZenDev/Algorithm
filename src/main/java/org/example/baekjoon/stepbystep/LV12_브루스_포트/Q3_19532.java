ae4d9a305d3d14e310365634071decc880c9a686
SerenityZenDev <tjrqo2000@gmail.com>
2024-05-19 10:46:45 +0900

    :sparkles: baekjoon(19532) - 수학은 비대면강의입니다

---
+++ src/main/java/org/example/baekjoon/stepbystep/LV12_브루스_포트/Q3_19532.java
@@ -0,0 +1,28 @@
+package org.example.baekjoon.stepbystep.LV12_브루스_포트;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Q3_19532 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+        int d = Integer.parseInt(st.nextToken());
+        int e = Integer.parseInt(st.nextToken());
+        int f = Integer.parseInt(st.nextToken());
+        // ax + by = c
+        // dx + ey = f
+
+        int x = (c * e - b * f) / (a * e - b * d);
+        int y = (c * d - a * f) / (b * d - a * e);
+
+        System.out.println(x + " " + y);
+    }
+}
