ae103a7295fcf768fefcce525e3f2fe650f400ee
SerenityZenDev <tjrqo2000@gmail.com>
2024-06-06 10:20:29 +0900

    :sparkles: baekjoon(1735) - 분수합

--- src/main/java/org/example/baekjoon/stepbystep/LV15_약수_배수와_소수_2/Q3_1735.java
+++ src/main/java/org/example/baekjoon/stepbystep/LV15_약수_배수와_소수_2/Q3_1735.java
@@ -1,2 +1,50 @@
-package org.example.baekjoon.stepbystep.LV15_약수_배수와_소수_2;public class Q3_1735 {
+package org.example.baekjoon.stepbystep.LV15_약수_배수와_소수_2;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Q3_1735 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        int C = Integer.parseInt(st.nextToken());
+        int D = Integer.parseInt(st.nextToken());
+
+        int numerator = A * D + B * C;
+        int denominator = B * D;
+
+        int mod = gcd(numerator, denominator);
+        numerator /= mod;
+        denominator /= mod;
+
+        bw.write(numerator + " " + denominator + "\n");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+    // 유클리드 호제법
+    public static int gcd(int a, int b) {
+        // a가 b보다 큰 경우에 대해서 유클리드 호제법 수행.
+        if (a <= b) {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+
+        if (b == 0) {
+            return a;
+        }
+        return gcd(b , a % b);
+    }
 }
