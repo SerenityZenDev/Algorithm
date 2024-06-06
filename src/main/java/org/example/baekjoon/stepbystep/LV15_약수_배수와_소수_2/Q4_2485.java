d0012ea1ba545ccd57398ddd532c5b3f54d2c6c6
SerenityZenDev <tjrqo2000@gmail.com>
2024-06-06 10:31:11 +0900

    :sparkles: baekjoon(2485) - 가로수

--- src/main/java/org/example/baekjoon/stepbystep/LV15_약수_배수와_소수_2/Q4_2485.java
+++ src/main/java/org/example/baekjoon/stepbystep/LV15_약수_배수와_소수_2/Q4_2485.java
@@ -1,2 +1,38 @@
-package org.example.baekjoon.stepbystep.LV15_약수_배수와_소수_2;public class Q4_2485 {
+package org.example.baekjoon.stepbystep.LV15_약수_배수와_소수_2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Q4_2485 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int size = Integer.parseInt(br.readLine());
+        int[] tree = new int[size];
+
+        for (int i = 0; i < size; i++) {
+            tree[i] = Integer.parseInt(br.readLine());
+        }
+
+        br.close();
+        int gcd = 0;
+
+        for (int i = 1; i < size - 1; i++) {
+            int distance = tree[i + 1] - tree[i];
+            gcd = findGCD(distance, gcd);
+        }
+
+        System.out.println((tree[size - 1] - tree[0])/gcd + 1 - (tree.length ));
+    }
+
+    private static int findGCD(int A, int B) {
+        while (B != 0) {
+            int R = A % B;
+            A = B;
+            B = R;
+        }
+        return A;
+    }
 }
