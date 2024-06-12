4c5b13ca67ab1a3fc3f77b30649ceda0c575f246
SerenityZenDev <tjrqo2000@gmail.com>
2024-06-12 15:45:56 +0900

    :sparkles: baekjoon(4948) - 베르트랑 공준

--- src/main/java/org/example/baekjoon/stepbystep/LV15_약수_배수와_소수_2/Q7_4948.java
+++ src/main/java/org/example/baekjoon/stepbystep/LV15_약수_배수와_소수_2/Q7_4948.java
@@ -1,2 +1,53 @@
-package org.example.baekjoon.stepbystep.LV15_약수_배수와_소수_2;public class Q7_4948 {
+package org.example.baekjoon.stepbystep.LV15_약수_배수와_소수_2;
+
+import java.util.Scanner;
+
+public class Q7_4948 {
+
+    static boolean[] flag;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n, nn = 0;
+        while (true) {
+            n = sc.nextInt();
+            if (n == 0) {
+                break;
+            } else if (n == 1){
+                System.out.println(1);
+                continue;
+            }
+
+            nn = n * 2;
+            flag = new boolean[nn];
+
+            makeFlag(flag, nn);
+            int result = 0;
+
+            for (int i = n + 1; i < nn; i++) {
+                if (!flag[i]) {
+                    result++;
+                }
+            }
+
+            System.out.println(result);
+        }
+
+
+    }
+
+    private static void makeFlag(boolean[] flag, int size) {
+
+        flag[0] = flag[1] = true;
+        if (size < 2) return;
+
+        for (int i = 2; i <= Math.sqrt(size); i++) {
+            if (flag[i]) {
+                continue;
+            }
+            for (int j = i * i; j < size; j+=i) {
+                flag[j] = true;
+            }
+        }
+    }
 }
