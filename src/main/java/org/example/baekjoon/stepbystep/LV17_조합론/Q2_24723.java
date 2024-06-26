e7cd80c65ec23a8620de8b4ed792ad6ab7656af1
SerenityZenDev <tjrqo2000@gmail.com>
2024-06-26 09:40:38 +0900

    :sparkles: baekjoon(24723) - 녹색거탑

---
+++ src/main/java/org/example/baekjoon/stepbystep/LV17_조합론/Q2_24723.java
@@ -0,0 +1,13 @@
+package org.example.baekjoon.stepbystep.LV17_조합론;
+
+import java.util.Scanner;
+
+public class Q2_24723 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        System.out.println((int) Math.pow(2, n));
+    }
+}
